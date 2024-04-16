17122d56f61a5e2e5d52a048eddb5c9c380f0afb
rafa1510 <rcestti78@gmail.com>
2024-04-16 00:14:28 -0400

    feature: fetch pokemon data from api
    will feed in this data to card props

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Navbar({ currentScore = 0, highScore = 0 }: { currentScore: number; highScore: number }) {
+export default function Navbar({ score = 0, highScore = 0 }: { score: number; highScore: number }) {
   return (
     <div className="navbar flex justify-between">
       <h1 className="btn btn-ghost btn-lg text-3xl">PokeGuess</h1>
       <div className="stats stats-vertical sm:stats-horizontal">
         <div className="stat">
           <h1 className="stat-title">Score:</h1>
-          <h2 className="stat-value">{currentScore}</h2>
+          <h2 className="stat-value">{score}</h2>
         </div>
         <div className="stat">
           <h1 className="stat-title">High Score:</h1>
