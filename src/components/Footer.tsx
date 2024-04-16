bb91f7ed18852095fbd138e68ff040b2e82dcaa4
rafa1510 <rcestti78@gmail.com>
2024-04-16 01:24:16 -0400

    chore: add footer to git

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto p-5 pb-2.5">
+      <aside>
+        <p> Made with &lt;3 by Rafael Cestti</p>
+      </aside>
+    </footer>
+  )
+}
