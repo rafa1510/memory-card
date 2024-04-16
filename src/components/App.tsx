fe82557af0b2040cd0e0e5dc0c9fd60e54466d8f
rafa1510 <rcestti78@gmail.com>
2024-04-16 00:33:12 -0400

    feature: implement score and high score

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import Navbar from './Navbar'
+import Card from './Card'
+
+interface Pokemon {
+  image: string
+  name: string
+}
 
 export default function App() {
+  const [pokemons, setPokemons] = useState(Array<Pokemon>)
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+
+  const [clickedIndexes, setClickedIndexes] = useState(new Set<number>([]))
+
+  function handleTurn(index: number) {
+    if (clickedIndexes.has(index)) {
+      if (clickedIndexes.size > highScore) {
+        setHighScore(clickedIndexes.size)
+      }
+      setScore(0)
+      setClickedIndexes(new Set([]))
+    } else {
+      const currentClickedIndexes = clickedIndexes
+      currentClickedIndexes.add(index)
+      setClickedIndexes(currentClickedIndexes)
+      setScore(score + 1)
+    }
+  }
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      const pokemonNames = [
+        'ditto',
+        'pikachu',
+        'charmander',
+        'squirtle',
+        'lucario',
+        'greninja',
+        'bulbasaur',
+        'rayquaza',
+        'mewtwo',
+        'gyarados',
+      ]
+
+      try {
+        const pokemonsData = await Promise.all(
+          pokemonNames.map(async (name) => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            const pokemonObject = await response.json()
+            return {
+              image: pokemonObject.sprites.front_default,
+              name: pokemonObject.species.name,
+            }
+          })
+        )
+        setPokemons(pokemonsData)
+      } catch (error) {
+        console.error('Failed to fetch Pokemons: ', error)
+      }
+    }
+
+    fetchPokemons().catch((error) => {
+      console.error('Error fetching data: ', error)
+    })
+  }, [])
+
   return (
-    <>
-      <h1>Template repo!</h1>
-    </>
+    <div>
+      <Navbar score={score} highScore={highScore} />
+      {pokemons.map((pokemon, index) => (
+        <Card key={index} pokemonImage={pokemon.image} pokemonName={pokemon.name} index={index} handleClick={handleTurn} />
+      ))}
+    </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function Card({ pokemonImage, pokemonName }: { pokemonImage: string; pokemonName: string }) {
+export default function Card({
+  pokemonImage,
+  pokemonName,
+  index,
+  handleClick,
+}: {
+  pokemonImage: string
+  pokemonName: string
+  index: number
+  handleClick: Function
+}) {
   return (
-    <div className="avatar bg-primary rounded-full">
+    <button className="avatar bg-primary rounded-full" onClick={() => handleClick(index)}>
       {pokemonImage && pokemonName ? <img src={pokemonImage} alt={pokemonName} /> : <div className="skeleton h-16 w-16 rounded-full"></div>}
-    </div>
+    </button>
   )
 }
