f51ead5d8fc9e616341e0af275bf7d4fcb530baa
rafa1510 <rcestti78@gmail.com>
2024-04-15 23:07:36 -0400

    feature: add static card
    this will contain our pokemon

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className="avatar bg-primary rounded-full">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="ditto" />
+    </div>
+  )
+}
